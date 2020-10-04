5dfa94ee07dc86efac18f6d0d33aef12d26c582c
Zakatnov Sergey <vertu8987@mail.ru>
2020-10-04 21:41:39 +0300

    Created Home Work 2 script

--- HomeWork 1.py
+++ HomeWork 1.py
@@ -8,10 +8,5 @@ print("10+5 =", b)
 sec = int(input("Введите время в секундах:"))
 hrs = sec // 3600
 min = sec // 60
-sek = sec // 1
+sek = sec
 print('Часы: {0}; Минуты: {1}; Секунды: {2}'.format(hrs, min, sek))
-
-
-
-
-
---
+++ HomeWork 2.py
@@ -0,0 +1,2 @@
+result_list = [1, None, 25, 69, 'text']
+print(type(result_list))
